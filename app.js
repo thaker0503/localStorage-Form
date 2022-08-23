54bbc406ad5e3470ae4bff3eb98443618f85afbe
Yatharth Thaker <52533890+thaker0503@users.noreply.github.com>
2022-08-23 11:08:59 +0530

    qq

--- app.js
+++ app.js
@@ -9,16 +9,21 @@ const signUp = (e) => {
 
     let exist = formData.length && JSON.parse(localStorage.getItem('formData')).some(data => data.email.toLowerCase() == email.toLowerCase()); 
 
-    if(!exist){
-        formData.push({ name,email, pwd, isLoggedIn });
-        localStorage.setItem('formData', JSON.stringify(formData));
-        alert("Registered Successfully\nPlease Login");
-        location.href = "./login.html";
-    }
-    else{
-        alert("Ooopppssss... Duplicate found!!!\nYou have already signed up\nPlease Sign In");
-        location.href = "./login.html";
+    if (validateEmail(email) && validatePassword(pwd) && validateName(name)) {
+        if (!exist) {
+            formData.push({ name, email, pwd, isLoggedIn });
+            localStorage.setItem('formData', JSON.stringify(formData));
+            alert("Registered Successfully\nPlease Login");
+            location.href = "./login.html";
+        }
+        else {
+            alert("Ooopppssss... Duplicate found!!!\nYou have already signed up\nPlease Sign In");
+            location.href = "./login.html";
 
+        }
+    }
+    else {
+        alert("Please enter valid details");
     }
     e.preventDefault();
 }
@@ -28,7 +33,7 @@ function signIn(e) {
         email = userLoginForm['email'].value, 
         pwd = userLoginForm['password'].value;
     let formData = JSON.parse(localStorage.getItem('formData')) || [];
-    let exist = formData.length && JSON.parse(localStorage.getItem('formData')).some(data => data.email.toLowerCase() == email);
+    let exist = formData.length && formData.some(data => data.email.toLowerCase() == email.toLowerCase());
     if (!exist) {
         alert("User doesn't exist \n Please Register!!");
         location.href = "./index.html";
@@ -36,12 +41,12 @@ function signIn(e) {
     }
     else {
         console.log("parent else")
-        if (JSON.parse(localStorage.getItem('formData')).some(data => data.email.toLowerCase() == email && data.pwd.toLowerCase() == pwd)) {
+        if (formData.some(data => data.email.toLowerCase() == email && data.pwd.toLowerCase() == pwd)) {
             console.log("child If")
-            let index = formData.findIndex(data => data.email.toLowerCase() == email && data.pwd.toLowerCase() == pwd);
-            formData[index].isLoggedIn = true;
-            localStorage.setItem('formData', JSON.stringify(formData));
-            location.replace("./dashboard.html?" + index);
+            // let index = formData.findIndex(data => data.email.toLowerCase() == email && data.pwd.toLowerCase() == pwd);
+            // formData[index].isLoggedIn = true;
+            // localStorage.setItem('formData', JSON.stringify(formData));
+            // location.replace("./dashboard.html?" + index);
         }
         else {
             console.log("child else")
@@ -75,8 +80,28 @@ function displayUserDetails(index) {
     }
 }
 
+const closeBtn = document.getElementById('closeBtn');
+function closeBtnClick(e) {
+    e.preventDefault();
+    document.getElementById('userRegisterForm').reset();
+}
+    
 
 
+function validateEmail(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email);
+}
+
+function validatePassword(password) {
+    var re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+    return re.test(password);
+}
+
+function validateName(name) {
+    var re = /^[a-zA-Z ]{2,30}$/;
+    return re.test(name);
+}
 
 
 
